ba5761d563681d79c64bc3c0961567a1726765e2
Ali Erinc Ayaz <45971992+erincayaz@users.noreply.github.com>
2023-08-27 14:56:01 +0300

    Added color remover
    Added color remover and changed some code

---
+++ src/rgbConverter.ts
@@ -0,0 +1,18 @@
+export class RgbConverter {
+    componentToHex(c : number) {
+      let hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
+      
+    rgbToHex(rgb : string) {
+      let substr = rgb.substring(4, rgb.length - 1);
+      let rgbArr = substr.split(',');
+
+      let hexStr = "#";
+      for (let i = 0; i < 3; i++) {
+          hexStr += this.componentToHex(parseInt(rgbArr[i]));
+      }
+
+      return hexStr;
+    }    
+}
